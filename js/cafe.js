ae9da5645a67a83f036b56d28b4e8aceb6b5f209
Nikita Zubov <n.zubov@lipt-soft.ru>
2025-11-16 15:06:19 +0300

    #0002 Fix cart. Exclude pay.

--- js/cafe.js
+++ js/cafe.js
@@ -249,35 +249,44 @@
       Cafe.updateTotalPrice();
     },
     mainBtnClicked: function() {
-      if (!Cafe.canPay || Cafe.isLoading || Cafe.isClosed) {
+      if (Cafe.isLoading || Cafe.isClosed) {
         return false;
       }
-      if (Cafe.modeOrder) {
-        var comment = $('.js-order-comment-field').val();
-        var params = {
-          order_data: Cafe.getOrderData(),
-          comment: comment
-        };
-        if (!Telegram.WebApp.initData ||
-            !Telegram.WebApp.initData.user ||
-            !Telegram.WebApp.initData.user.id) {
-          params.user_id = Cafe.userId;
-          params.user_hash = Cafe.userHash;
-        }
-        Cafe.toggleLoading(true);
-        Cafe.apiRequest('makeOrder', params, function(result) {
-          Cafe.toggleLoading(false);
-          if (result.ok) {
-            Telegram.WebApp.close();
-          }
-          if (result.error) {
-            Cafe.showStatus(result.error);
-          }
-        });
-      } else {
-        Cafe.toggleMode(true);
-      }
+
+      // всегда просто переключаемся в режим корзины
+      // без оплаты, без запросов, без ошибок
+      Cafe.toggleMode(false);
     },
+    // mainBtnClicked: function() {
+    //   if (!Cafe.canPay || Cafe.isLoading || Cafe.isClosed) {
+    //     return false;
+    //   }
+    //   if (Cafe.modeOrder) {
+    //     var comment = $('.js-order-comment-field').val();
+    //     var params = {
+    //       order_data: Cafe.getOrderData(),
+    //       comment: comment
+    //     };
+    //     if (!Telegram.WebApp.initData ||
+    //         !Telegram.WebApp.initData.user ||
+    //         !Telegram.WebApp.initData.user.id) {
+    //       params.user_id = Cafe.userId;
+    //       params.user_hash = Cafe.userHash;
+    //     }
+    //     Cafe.toggleLoading(true);
+    //     Cafe.apiRequest('makeOrder', params, function(result) {
+    //       Cafe.toggleLoading(false);
+    //       if (result.ok) {
+    //         Telegram.WebApp.close();
+    //       }
+    //       if (result.error) {
+    //         Cafe.showStatus(result.error);
+    //       }
+    //     });
+    //   } else {
+    //     Cafe.toggleMode(true);
+    //   }
+    // },
     eStatusClicked: function() {
       Cafe.hideStatus();
     },
